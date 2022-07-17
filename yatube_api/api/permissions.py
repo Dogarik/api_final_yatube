a4c3857b84d97cca5670c988a8931e598b74ab2c
Rustam Magomedov <dogarik2007@yandex.ru>
2022-07-18 00:20:20 +0800

    Первый коммит после ревью

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -4,10 +4,6 @@ from rest_framework import permissions
 class OwnerOrReading(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        if (
-            request.user.is_superuser
-            or request.method in permissions.SAFE_METHODS
-            or obj.author == request.user
-        ):
-            return True
-        return False
+        return (request.user.is_superuser
+                or request.method in permissions.SAFE_METHODS
+                or obj.author == request.user)
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -46,10 +46,13 @@ class Comment(models.Model):
 
 class Follow(models.Model):
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='follower'
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower'
     )
     following = models.ForeignKey(
-        User, on_delete=models.CASCADE,
+        User,
+        on_delete=models.CASCADE,
         related_name='following'
     )
 
